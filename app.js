77faaa575298a360833a33f7fd6115960edb6f22
zopaj63 <zoran.pajnic@gmail.com>
2021-06-21 22:48:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,23 @@ console.log(fizzBuzz1);
 
 var fizzBuzz2 = findFizzBuzz(5, 40, "Hello", "World");
 console.log(fizzBuzz2);
+
+
+/*
+VJEŽBA: Funkcije, petlje i nizovi
+Trajanje: 30min (cca 20:55)
+
+1. Napravite novu .js datoteku
+2. Napišite JS program koji će definirati funkciju "findFizzBuzz"
+3. Funkcija "findFizzBuzz" će vratiti niz fizzbuzz poruka prema pravilima:
+3a. u niz "fizzBuzz" spremiti samo one vrijednosti u rasponu od 0 do parametra "max" (uključivo) koje su djeljive sa 3 i sa 5.
+3b. Ako je broj djeljiv sa 3 u "fizzBuzz" zapisati "fizz".
+3c. Ako je broj djeljiv sa 5 u "fizzBuzz" zapisati "buzz".
+3d. Ako je broj djeljiv sa 15 u "fizzBuzz" zapisati "fizzbuzz".
+3e. Sve ostale brojeve ne zapisujete u niz.
+4. Ispisati rezultat pozivanja "findFizzBuzz" sa argumentom "20" i "40".
+
+BONUS:
+1. Definirajte i "min" parametar uz "max" tako da možete definirati od kojeg i do kojeg broja se petlja kreće
+2. Definirajte parametre za "fizz" i "buzz" tekstove da možete sami definirati tekst koji će se ispisati kod djeljivosti
+*/
